d4fb28ca943909f423541fded56938d1780f207c
Jonathan Gao <jonathangao124@gmail.com>
2020-04-06 22:01:56 -0400

    added previous_login field to CustomUser, rewrote signal on login

--- users/models.py
+++ users/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth.signals import user_logged_in
 
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
+    previous_login = models.DateTimeField(null=True, blank=True)
+
+
+def update_previous_login(sender, user, **kwargs):
+    user.previous_login = user.last_login
+    user.last_login = timezone.now()
+    user.save(update_fields=['previous_login', 'last_login'])
+
+
+user_logged_in.disconnect(update_last_login, dispatch_uid='update_last_login')
+user_logged_in.connect(update_previous_login, dispatch_uid='update_previous_login')
