ad6922dec73f0b18f625a96b463cb2e475700881
Jonathan Gao <jonathangao124@gmail.com>
2020-04-17 07:32:54 -0400

    added unit description to model and template

--- units/models.py
+++ units/models.py
@@ -20,6 +20,7 @@ import json
 class Unit(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=255)
     lessons = models.ManyToManyField(Lesson, related_name='units')
     start_skills = JSONField(blank=True, null=True)
     end_skills = JSONField(blank=True, null=True)
